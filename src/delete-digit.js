dda47872581d62b1b2e7b046d080d3744a887027
googray <shat.rom07@gmail.com>
2022-03-23 13:22:45 +0100

    add character count & Delete digit

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given some integer, find the maximal number you can obtain
@@ -11,11 +11,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let res = 0;
+  let i = 1;
+  while (parseInt(n / i) > 0) {
+    let temp = parseInt(n / (i * 10)) * i + (n % i);
+    i *= 10;
+    res = Math.max(res, temp);
+  }
+  n = res;
+  return n;
 }
 
 module.exports = {
-  deleteDigit
+  deleteDigit,
 };
